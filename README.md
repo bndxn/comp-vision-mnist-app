0426c2d151b9ea11eecd59951b1278ae5cea4f19
Ben Dixon <uk.bendixon@gmail.com>
2023-12-21 11:44:46 +0000

    fashion mnist experiment

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # comp-vision-mnist-app
 An app which receives fashion-mnist images and returns the class
+
+* Fashion MNIST data set on Kaggle [here](https://www.kaggle.com/datasets/zalando-research/fashionmnist)
+* Pytorch model taken from Pytorch quickstart [here](https://pytorch.org/tutorials/beginner/basics/quickstart_tutorial.html)
+* FastAPI
+
+## Potential extensions
+
+* Squirrel spotter? Train on this dataset: [Squirrel images](https://www.kaggle.com/datasets/harrybaines/squirrels)
+* Could also combine with langchain! Getting a bit ahead of myself maybe
---
+++ fashion_mnist/inference_mnist_model.py
@@ -0,0 +1,42 @@
+# From here: https://pytorch.org/tutorials/beginner/basics/quickstart_tutorial.html
+
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+from train_mnist_model import NeuralNetwork, test_data
+
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+print(f"Using {device} device")
+
+model = NeuralNetwork().to(device)
+model.load_state_dict(torch.load("model.pth"))
+
+classes = [
+    "T-shirt/top",
+    "Trouser",
+    "Pullover",
+    "Dress",
+    "Coat",
+    "Sandal",
+    "Shirt",
+    "Sneaker",
+    "Bag",
+    "Ankle boot",
+]
+
+model.eval()
+x, y = test_data[0][0], test_data[0][1]
+with torch.no_grad():
+    x = x.to(device)
+    pred = model(x)
+    predicted, actual = classes[pred[0].argmax(0)], classes[y]
+    print(f'Predicted: "{predicted}", Actual: "{actual}"')
